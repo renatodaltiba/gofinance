182edf71b190d4179774d9b91ee4ccf29262096a
Renato Daltiba Schneider <renatodaltiba@gmail.com>
2021-05-25 08:52:15 -0400

    first commit

---
+++ src/components/TransactionCard/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native'
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
+
+export const Container = styled.View``
+export const Title = styled.Text``
+export const Amount = styled.Text``
+export const Footer = styled.View``
+export const Category = styled.View``
+export const Icon = styled.View``
+export const CategoryName = styled.Text``
+export const Date = styled.Text``
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,25 @@
+export default {
+  colors: {
+    primary: '#5636e3',
+
+    secondary: '#ff872c',
+    secondary_light: 'rgba(255, 135, 44, 0.3)',
+
+    success: '#12a454',
+    success_light: 'rgba(18, 164, 84, 0.5)',
+
+    attention: '#E83F5B',
+    attention_light: 'rgba(232, 63, 91, 0.5)',
+
+    shape: '#ffffff',
+    title: '#363F5F',
+    text: '#969CB3',
+    background: '#F0F2F5',
+  },
+
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  },
+}
